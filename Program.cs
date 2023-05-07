8dbc3d8b29ddbece9cfecb30a077e691bfdf870b
Eric Muganga <105128548+eric-muganga@users.noreply.github.com>
2023-05-07 10:44:23 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// Time complexity: O(n) and Space complexity: O(1)
+
+string input = Console.ReadLine();
+
+Console.WriteLine(IsPalindrome(input));
+
+bool IsPalindrome(string str)
+{
+    for (int i = 0; i < str.Length / 2; i++)
+    {
+        if (str[i] != str[str.Length - (i + 1)])
+        {
+            return false;
+        }
+    }
+    return true;
+}
